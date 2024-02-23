0a4777da11d6a8dc7124f8ac849235c716a5f5df
Russell NMcCloy <russell.mccloy@googlemail.com>
2024-02-22 21:04:25 -0300

    y

--- main.tf
+++ main.tf
@@ -1,4 +1,33 @@
 resource "azurerm_resource_group" "this" {
-  name     = local.config.resource_groups.name
-  location = local.config.resource_groups.location
+  name     = local.config.resource_group.name
+  location = local.config.resource_group.location
+}
+
+resource "azurerm_search_service" "this" {
+  name                = local.config.azurerm_search_service.name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  sku                 = "basic"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = local.config.azurerm_storage_account.name
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_storage_container" "this" {
+  name                  = local.config.azurerm_storage_account.name
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = "private"
 }
---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "identity_principal_id" {
+  value = azurerm_search_service.this.identity[0].principal_id
+}
+
+output "azurerm_storage_account_id" {
+  value     = azurerm_storage_account.this.id
+  sensitive = false
+}
