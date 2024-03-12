1e120c7930520a5aa55bdb7f6a7a41a4203857d6
Russell NMcCloy <russell.mccloy@googlemail.com>
2024-03-12 17:08:22 -0300

    skillset

--- scripts/Create-Skillset.ps1
+++ scripts/Create-Skillset.ps1
@@ -168,5 +168,5 @@ $body = @"
 }
 "@
 
-$response = Invoke-RestMethod "$($searchServiceUrl)/skillsets/$($indexName)-skillset?api-version=2023-11-01" -Method 'POST' -Headers $headers -Body $body
+$response = Invoke-RestMethod "$($searchServiceUrl)/skillsets/$($indexName)-skillset?api-version=2023-11-01" -Method 'PUT' -Headers $headers -Body $body
 $response | ConvertTo-Json
