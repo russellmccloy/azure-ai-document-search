9ad6f24942ebe07f0377ff552b39d1f8f3a38341
Russell NMcCloy <russell.mccloy@googlemail.com>
2024-03-12 18:22:41 -0300

    .NET 8

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_search_service" "this" {
   name                = local.config.azurerm_search_service.name
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
-  sku                 = "free"
+  sku                 = "basic"
 
   identity {
     type = "SystemAssigned"
@@ -32,6 +32,12 @@ resource "azurerm_storage_container" "this" {
   name                  = local.config.azurerm_storage_container.name
   storage_account_name  = azurerm_storage_account.this.name
   container_access_type = "private"
+}
+
+resource "azurerm_role_assignment" "this" {
+  scope                = azurerm_storage_account.this.id
+  role_definition_name = "Storage Blob Data Reader" # We need this so the Azure AI Search service can read the Storage Account
+  principal_id         = azurerm_search_service.this.identity[0].principal_id
 }
 
 # resource "azurerm_cognitive_account" "this" {
