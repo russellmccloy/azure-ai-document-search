75cf28ee75efa548748278ef28bce6a99876c83f
Russell NMcCloy <russell.mccloy@googlemail.com>
2024-03-13 17:41:17 -0300

    add all MVC code to GitHub for now

---
+++ MVCFrontEnd/MvcAzureAISearch/Models/SearchResult.cs
@@ -0,0 +1,33 @@
+    using System.Text.Json.Serialization;
+namespace MvcAzureAISearch.Models;
+
+using Newtonsoft.Json;
+
+// A model that represents the search data that is returned
+public class SearchResult
+    {
+        [JsonProperty("@odata.context")]
+        public string? OdataContext { get; set; }
+
+        [JsonProperty("@odata.count")]
+        public int? OdataCount { get; set; }
+
+        [JsonProperty("value")]
+        public List<Details>? Details { get; set; }
+    }
+
+    public class Details
+    {
+        [JsonProperty("@search.score")]
+        public double? SearchScore { get; set; }
+
+        [JsonProperty("language")]
+        public string? Language { get; set; }
+
+        [JsonProperty("organizations")]
+        public List<object>? Organizations { get; set; }
+
+        [JsonProperty("metadata_storage_name")]
+        public string? MetadataStorageName { get; set; }
+    }
+
